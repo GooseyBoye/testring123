b8908e710768956f757265d0849f5dc4e603ac8b
GooseyBoye <zurgarhu@hotmail.com>
2023-02-01 19:32:04 -0500

    Examples from in class

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
+
 import logo from './logo.svg';
-import './App.css';
+//import './App.css';
+
+function MyButton() {
+  function handleClick() {
+    alert('You clicked me!');
+  }
+  return (
+    <button>I'm a button</button>
+    
+  );
+}
 
 function App() {
+  const name = "Zuriel"
+const user = {
+  name: 'Hedy Lamarr',
+  imageUrl: 'https://i.imgur.com/yXOvdOSs.jpg',
+  imageSize: 90,
+};
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello <p style = { {backgroundColor: "red"} }>{name}!</p></h1>
+      <MyButton />
+      <p>More text being returned </p>
+      <img
+        className="avatar"
+        src={user.imageUrl}
+        alt={'Photo of ' + user.name}
+        style={{
+          width: user.imageSize,
+          height: user.imageSize
+        }}
+      />
+
+      
     </div>
   );
 }
 
 export default App;
+
+// function Example() {
+//   const [showModal, setShowModal] = useState(false);
+
+//   return (
+//     <div>
+//       <button onClick={() => setShowModal(true)}>Open Modal</button>
+//       {showModal && (
+//         <div style={{ background: 'white', padding: 20 }}>
+//           <h3>Modal Title</h3>
+//           <p>Modal Content</p>
+//           <button onClick={() => setShowModal(false)}>Close</button>
+//         </div>
+//       )}
+//     </div>
+//   );
+// }
+
+// export default Example;
